bae464f1760211cff258b6f2ea0cdded29a787f1
VN-78 <vishwanath.s.god@gmail.com>
2024-08-27 21:07:09 +0000

    working but sql does not works with this

--- M_1/app.py
+++ M_1/app.py
@@ -1,7 +1,25 @@
-from flask import Flask , render_template ,url_for , send_from_directory
+from flask import Flask , render_template ,url_for , send_from_directory , request , redirect 
+from flask_bcrypt import Bcrypt
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
 
 
 app = Flask(__name__)
+app.secret_key ="I_Am_Jesus"
+
+
+# connecting to database
+
+app.config['MYSQL_PORT'] =3306
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'mydb'
+
+# initialize the database & password
+mysql = MySQL(app)
+bcrypt = Bcrypt(app)
+
 
 @app.route('/')
 def home():
@@ -12,10 +30,32 @@ def home():
 def resume():
     return send_from_directory(directory='static' , path='My_Resume.pdf')
 
-@app.route('/register')
+@app.route('/register' , methods =['POST','GET'])
 def register():
-    return render_template('register.html')
+        if request.method == 'POST' :
+            username = request.form.get('username')
+            email = request.form.get('email')
+            password = request.form.get('password')
 
+            # hash the password
+            hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+            # create cursor object
+            cursor= mysql.connection.cursor()
+
+            # insert data to database
+            cursor.execute("INSERT INTO users (username, email, password_hash) VALUES (%s, %s, %s)", (username, email, hashed_password))
+
+            # commit the transaction
+            mysql.connection.commit()
+
+            # close the connection
+            cursor.close()
+
+            return "User registered successfully!"
+            
+            return redirect (url_for('login'))
+        return render_template('register.html')
 
 
 if __name__ == '__main__':
